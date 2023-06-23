f133032a8d67763f15fb60dc215835961817f654
Vilas <vilas.dodiya@iflair.com>
2023-06-23 18:38:49 +0530

    Multiple option changes-In progress

--- resources/views/forms/createForm.blade.php
+++ resources/views/forms/createForm.blade.php
@@ -65,6 +65,7 @@
                                             <div class="flex-grow-1 ">
                                                 <div class="question_type_dropdown">
                                                     <select class="form-control question_type questionTypeValue">
+                                                        <option value="">Select Question Type</option>
                                                         @foreach($inputTypesData as $inputType)
                                                         <option value="{{$inputType->id}}" data-inputType="{{$inputType->input_type}}">{{$inputType->input_type}}</option>
                                                         @endforeach
@@ -81,6 +82,7 @@
                             </div>
                         </div>
                     </div>
+
                     <!--begin::Actions-->
                     <div class="d-flex flex-stack pt-10">
                         <div>
@@ -126,7 +128,7 @@
                     appendInput+='<span>-</span>';
                     appendInput+='</div>';
                     appendInput+='<div class="col-md-10">';
-                    appendInput+='<div class="question_type_options">';
+                    appendInput+='<div class="question_type_options first_multiple_option">';
                     appendInput+='<input type="text" class="form-control form-control-lg form-control-solid multiOption" name="multipleChoice" value="Option 1" data-inputType = "'+questionInputTypeValue+'"/>';
                     appendInput+='</div>';
                     appendInput+='</div>';
@@ -144,32 +146,22 @@
                     appendInput+='</div>';
                     appendInput+='<div class="col-md-2">';
                     appendInput+='<div class="question_type_options">';
-                    appendInput+='<input type="text" class="form-control form-control-sm form-control-solid multiOption LastMultipleOption" name="multipleChoice" placeholder="Add Option" value="" />';
+                    appendInput+='<a href="javascript:void(0)" class="btn btn-primary form-control form-control-sm form-control-solid multiOption LastMultipleOption" name="multipleChoice" >Add Option</a>';
                     appendInput+='</div>';
-                    appendInput+='</div>';
-                    appendInput+='<div class="col-md-2">';
-                    appendInput+='<span>Or </span>';
-                    appendInput+='<a href="javascript:void(0)">Add "Other"</a>';
                     appendInput+='</div>';
                     appendInput+='</div>';
                     appendInput+='</div>';
                     appendInput+='</div>';
 
-
-                    /* appendInput+='<div class="removeLastOption"><span>-</span>';
-                    appendInput+='<div class="question_type_options"><input type="text" class="form-control form-control-lg form-control-solid multiOption" name="multipleChoice" value="Option 1" data-inputType = "'+questionInputTypeValue+'"/><a href="" class="removeMultipleOption"><i class="fa fa-times" aria-hidden="true"></i></a></div>';
-                    appendInput+='<div class="multipleOptionAppend"></div>';
-                    appendInput+='<div class="lastOption"><span>-</span>';
-                    appendInput+='<div class="question_type_options"><input type="text" class="form-control form-control-lg form-control-solid LastMultipleOption multiOption" name="multipleChoice" placeholder="Add Option" value="" /><span>Or </span><a href="">Add "Other"</a></div></div></div>'; */
                 }
                 $('.appendInput').append(appendInput);
                 
 
                 $(".LastMultipleOption").click(function(){
                     var multipleOptionAppend = '';
-                    var countOption = $('.multiOption').length;
+                    var countOption = $('.appendInput .multiOption').length;
 
-                    multipleOptionAppend+='<div class="row mb-2">';
+                    multipleOptionAppend+='<div class="row mb-2 middleOptions">';
                     multipleOptionAppend+='<div class="col-md-1">';
                     multipleOptionAppend+='<span>-</span>';
                     multipleOptionAppend+='</div>';
@@ -187,9 +179,9 @@
 
 
                     /* multipleOptionAppend+='<span>-</span>';
-                    multipleOptionAppend+='<div class="question_type_options"><input type="text" class="form-control form-control-lg form-control-solid multiOption" name="multipleChoice" value="Option '+ countOption +'" data-inputType = "'+questionInputTypeValue+'"/><a href="" class="removeMultipleOption"><i class="fa fa-times" aria-hidden="true"></i></a></div>'; */
+                    multipleOptionAppend+='<div class="question_type_options"><input type="text" class="form-control form-control-lg form-control-solid multiOption" name="multipleChoice" value="Option '+ countOption +o'" data-inputType = "'+questionInputTypeValue+'"/><a href="" class="removeMultipleOption"><i class="fa fa-times" aria-hidden="true"></i></a></div>'; */
 
-                    $('.multipleOptionAppend').append(multipleOptionAppend);
+                    $('.appendInput .multipleOptionAppend').append(multipleOptionAppend);
 
                 });
 
@@ -213,15 +205,46 @@
                     
                 var questionTypeValue = $('.questionTypeValue').attr("data-inputtype");
                 var questionTypeAppend = '';
-                questionTypeAppend+='<div class="questionAppend"><span>'+questionValue+'</span></div>'+appendInput+'</div><div><a href="" class="btn getDuplicateQuestion"><i class="fa fa-clone"></i></a><a href="javascript:void(0)" class="btn removeQuestion"><i class="fa fa-trash"></i></a><label for="required_toggle">Required </label><input type="checkbox" value="1" checked></div>';
+
+                questionTypeAppend+='<div class="card mb-5">';
+                questionTypeAppend+='<div class="card-body">';
+                questionTypeAppend+='<div class="question_Details">';
+                questionTypeAppend+='<div class="row mb-5">';
+                questionTypeAppend+='<div class="col-md-8">';
+                questionTypeAppend+='<div class="questionAppend">'+questionValue+'</div>';
+                questionTypeAppend+='</div>';
+                questionTypeAppend+='<div class="col-md-4">';
+                questionTypeAppend+='<a href="javascript:void(0)" class="btn getDuplicateQuestion">';
+                questionTypeAppend+='<i class="fa fa-clone"></i>';
+                questionTypeAppend+='</a>';
+                questionTypeAppend+='<a href="javascript:void(0)" class="btn removeQuestion">';
+                questionTypeAppend+='<i class="fa fa-trash"></i>';
+                questionTypeAppend+='</a>';
+                questionTypeAppend+='<label for="required_toggle">Required </label>';
+                questionTypeAppend+='<input type="checkbox" value="1" checked>';
+                questionTypeAppend+='</div>';
+                questionTypeAppend+='</div>';
+
+                questionTypeAppend+='<div class="row  mb-5">';
+                questionTypeAppend+='<div class="col-md-12"> '+appendInput+' </div>';
+                questionTypeAppend+='</div>'; 
+                questionTypeAppend+='</div>';
+                questionTypeAppend+='</div>';
+                questionTypeAppend+='</div>';
+
+                /* questionTypeAppend+='<div class="questionAppend"><span>'+questionValue+'</span></div>'+appendInput+'</div><div><a href="" class="btn getDuplicateQuestion"><i class="fa fa-clone"></i></a><a href="javascript:void(0)" class="btn removeQuestion"><i class="fa fa-trash"></i></a><label for="required_toggle">Required </label><input type="checkbox" value="1" checked></div>'; */
 
                 $('.questionDetailBox').append(questionTypeAppend);
                 $('.questionValue').val('');
-                // $('.questionTypeValue').val('');
+                // $(".appendInput .middleOptions").remove();
+                $('.appendInput').empty();
+                $('.questionTypeValue').val('');
 
             });
 
-            $(".removeQuestion").click(function(e){
+            removeMultipleOption
+            
+            $(".removeMultipleOption").click(function(e){
                 e.preventDefault();
                 alert("test");
                 $(this).parent(".questionDetailBox").remove();
